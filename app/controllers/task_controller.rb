af17e56782fd82735916df7a0c1e6501a7248bbd
keesha <kharg14@gmail.com>
2020-10-16 22:57:47 -0400

    update t.controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -23,16 +23,41 @@ get '/tasks' do
 end
 
 
-
-
 #update
+
+get '/tasks/:id/edit' do
+    @tasks = Task.find(params[:id])
+    erb :'/tasks/edit'
+
+end 
+
+#patch '/tasks/:id/' do
+   # @tasks = Task.find(params[:id])
+   # @tasks=Task.update(things_to_do: params[:things_to_do], notes:params[:notes])
+        
+   # redirect "/tasks/#{@tasks.id}"
+
+
+#end
+patch '/tasks/:id' do #edit action
+    @tasks = Task.find(params[:id])
+   @tasks.things_to_do = params[:things_to_do]
+    @tasks.notes = params[:notes]
+    
+    @tasks.save
+    redirect to "/tasks/#{@tasks.id}"
+  end
 
 
 
 
 #delete
 
-
+delete '/tasks/:id' do #delete action
+    @tasks = Task.find(params[:id])
+    @tasks.delete
+    redirect to '/tasks'
+  end
 
 
 
