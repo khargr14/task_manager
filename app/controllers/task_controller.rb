315800b5192bb503cf15e0df545081677573d3e3
keesha <kharg14@gmail.com>
2020-10-16 20:35:25 -0400

    update t.controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -2,17 +2,20 @@ class TaskController < ApplicationController
 
 #create
 
-    get '/tasks/new' do  # objests
+    get '/tasks/new' do  
         erb :'/tasks/new'
     end
 
-    post '/tasks/new' do
-       @task = @tast.create(things_to_do: params[:things_to_do], notes: params[:notes], due_date: params[:due_date] ) "process our form"
-    end
-
+    post '/tasks' do
+       @tasks = Task.create(things_to_do: params[:things_to_do], notes:params[:notes], due_date:params[:due_date]) #"process our form"
+       redirect "/tasks/#{@tasks.id}"
+    end 
 
 #read
-
+get '/tasks/:id' do
+    @tasks = Task.find(params[:id])
+    erb :'/tasks/show'
+end
 
 
 #update
@@ -21,7 +24,6 @@ class TaskController < ApplicationController
 
 
 #delete
-
 
 
 
